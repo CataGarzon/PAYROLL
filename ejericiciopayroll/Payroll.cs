0a51a1bc42f66035d7bb6034cd807999270d2a7d
CataGarzon <95449594+CataGarzon@users.noreply.github.com>
2022-06-13 17:46:30 -0500

    Add files via upload

---
+++ ejericiciopayroll/Payroll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejericiciopayroll
+{
+    class Payroll
+    {
+        private double Document;
+        private double WorkedDays;
+        private double Salary;
+        private string FirstName;
+        private string LastName;
+     
+        public void setDocument(double Document_)
+        { this.Document = Document_; }
+        public double getDocument () 
+        { return this.Document; }
+       
+        public void setWorkedDays(double WorkedDay_)
+        { this.WorkedDays = WorkedDay_; }
+
+        public double getWorkedDays()
+        { return this.WorkedDays; }
+
+        public void setSalary(double Salary_)
+        { this.Salary = Salary_; }
+
+        public double getSalary()
+        { return this.Salary;}
+
+        public void setFirstName(string FirstName_)
+        { this.FirstName = FirstName_; }
+        
+        public string getFirtName()
+        { return this.FirstName; }
+
+        public void setLastName(string LastName_)
+        { this.LastName = LastName_; }
+
+        public string getLastNme()
+        { return this.LastName; }
+    }
+}
---
+++ ejericiciopayroll/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ejericiciopayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Payroll myObj = new Payroll();
+                Console.WriteLine("Insert document: ");
+                myObj.setDocument(double.Parse(Console.ReadLine()));
+                Console.WriteLine("*************************************");
+
+                
+                Console.WriteLine("Insert First Name: ");
+                myObj.setFirstName(Console.ReadLine());
+                Console.WriteLine("*************************************");
+
+                Console.WriteLine("Insert Last Name: ");
+                myObj.setLastName(Console.ReadLine());
+                Console.WriteLine("*************************************");
+
+              
+                Console.WriteLine("Insert Salary: ");
+                myObj.setSalary(double.Parse(Console.ReadLine()));
+                Console.WriteLine("*************************************");
+
+
+                Console.WriteLine("Insert Worked Days: ");
+                myObj.setWorkedDays(double.Parse(Console.ReadLine()));
+                Console.WriteLine("*************************************");
+
+
+
+
+
+
+
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
